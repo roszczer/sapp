786286eca7198c3d417a343c461043e8abc457cc
Robert Szczerbak <roszczer@gmail.com>
2012-05-14 13:15:59 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Sapp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
   
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name	"rob"
+    email	"rob@ex.com"
+    password	"tuptus"
+    password_confirmation "tuptus"
+  end
+end
